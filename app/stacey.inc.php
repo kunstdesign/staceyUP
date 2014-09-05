336fec1d531a20d79495794dc62ccfb88c1c919b
Leonardo Dino <leonardo@kunstdesign.com.br>
2014-09-05 01:28:10 -0300

    Set timezone to America/Sao_Paulo

--- app/stacey.inc.php
+++ app/stacey.inc.php
@@ -24,7 +24,7 @@ Class Stacey {
 
   function php_fixes() {
     # in PHP/5.3.0 they added a requisite for setting a default timezone, this should be handled via the php.ini, but as we cannot rely on this, we have to set a default timezone ourselves
-    if(function_exists('date_default_timezone_set')) date_default_timezone_set('Australia/Melbourne');
+    if(function_exists('date_default_timezone_set')) date_default_timezone_set('America/Sao_Paulo');
   }
 
   function set_content_type($template_file) {
